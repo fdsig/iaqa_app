d983f5c9fb24e0ba2be4b5d05bdb4d59de5ab4e3
fdsig <fdesigley@gmail.com>
2022-01-31 16:41:37 +0000

    adding app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import url_for
+from flask import render_template 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/test')
+def app_test():
+    test = ['hello' for i in range(10000)]
+    return 'hello'+ f"{['hello' for i in range(10000)]}"
+
+@app.route('/hello')
+def hello():
+    return 'hello'
+
+with app.test_request_context():
+    print(url_for('index'))
