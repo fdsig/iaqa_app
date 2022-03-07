15019524629cb524a5317106cd3bf5e3e17dbe8f
fdsig <fdesigley@gmail.com>
2022-03-07 18:01:51 +0000

    adding infer as class

--- predict.py
+++ predict.py
@@ -7,38 +7,41 @@ import torch.nn.functional as F
 import torch.nn as nn
 import timm
 
+class Convit:
+    def __init__(self):
+        self.model, self.device = self.load()
+        self.preprocess =  A.Compose([
+                A.augmentations.geometric.resize.LongestMaxSize(max_size=224),
+                A.augmentations.transforms.PadIfNeeded(224,224),
+                A.augmentations.transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), 
+                                                    max_pixel_value=255.0, p=1.0),
+                                A.pytorch.transforms.ToTensorV2(transpose_mask=False, p=1.0) ]
+                                )
+        self.device = torch.device('cpu')
+        self.model_name = 'convit_tiny'
 
-def load():
-    device = torch.device('cpu')
-    weights = torch.load('convit_tiny.pth', map_location=device)
-    weights = weights['model']
-    model_name = 'convit_tiny'
-    model = timm.create_model(model_name , pretrained=True)
-    model.head = nn.Linear(model.head.in_features,2,bias=True)
-    model.load_state_dict(weights)
-    return model, device
-
-a_test_transform = A.Compose([
-            A.augmentations.geometric.resize.LongestMaxSize(max_size=224),
-            A.augmentations.transforms.PadIfNeeded(224,224),
-            A.augmentations.transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), 
-                                                max_pixel_value=255.0, p=1.0),
-                            A.pytorch.transforms.ToTensorV2(transpose_mask=False, p=1.0) ]
-                            )
-def read(img):return cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB)
-fid = '730394.jpg'
-img = read(fid)
+    def load(self):
+        device = torch.device('cpu')
+        weights = torch.load('convit_tiny.pth', map_location=device)
+        weights = weights['model']
+        model = timm.create_model(self.model_name , pretrained=True)
+        model.head = nn.Linear(model.head.in_features,2,bias=True)
+        model.load_state_dict(weights)
+        return model, device
 
 
-model,device = load()
-img_normalized = a_test_transform(image=img)['image'].float()
-img_normalized = img_normalized.unsqueeze_(0)
-   # input = Variable(image_tensor)
-img_normalized = img_normalized.to(device)
-   # print(img_normalized.shape)
-with torch.no_grad():
-    model.eval()  
-    output = model(img_normalized)
-    sm = torch.nn.Softmax(dim=1)
-    print(sm(output))
+    def read(self,img):return cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB)
+
+    def infer(self, img):
+        img = self.read(img)
+        img_normalized = self.preprocess(image=img)['image'].float()
+        img_normalized = img_normalized.unsqueeze_(0)
+        # input = Variable(image_tensor)
+        img_normalized = img_normalized.to(self.device)
+        # print(img_normalized.shape)
+        with torch.no_grad():
+            model.eval()  
+            output = model(img_normalized)
+            sm = torch.nn.Softmax(dim=1)
+            return sm(output)
 
